e4b5ceea1d2ed86610cfb9a16e1e62669a753dbc
sjaymoon15 <sjaymoon15@gmail.com>
2017-09-02 09:41:46 -0700

    get width of device and set up rotation

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import { 
+  View, 
+  Animated, 
+  PanResponder,
+  Dimensions
+} from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Deck extends Component {
   constructor(props) {
@@ -22,7 +29,7 @@ class Deck extends Component {
   getCardStyle() {
     const { position } = this.state;
     const rotate = position.x.interpolate({
-      inputRange: [-500, 0, 500],
+      inputRange: [-SCREEN_WIDTH, 0, SCREEN_WIDTH],
       outputRange: ['-120deg', '0deg', '120deg']
     })
     return { 
